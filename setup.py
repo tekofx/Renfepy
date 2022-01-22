2434e69bf5f4f41b6387e30af2a1b4aafcd66626
Tekofx <tekofx@gmail.com>
2022-01-22 21:16:35 +0100

    Changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="renfepy",  # How you named your package folder (MyLib)
     packages=["renfepy"],  # Chose the same as "name"
-    version="1.6",  # Start with a small number and increase it with every change you make
+    version="1.7",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="A library to search for trains in renfe.com",  # Give a short description about your library
     author="Teko",  # Type in your name
@@ -13,9 +13,11 @@ setup(
     keywords=[
         "renfe",
         "train",
-    ],  # Keywords that define your package best
-    install_requires=[  # I get to this in a second
+    ],
+    install_requires=[
         "selenium",
+        "prettytable",
+        "webdriver-manager",
     ],
     classifiers=[
         "Development Status :: 4 - Beta",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
