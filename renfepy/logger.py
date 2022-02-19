06098cad0b7e94a7f453451905c063d9ebd4c5c9
Tekofx <--global>
2022-02-19 15:01:21 +0100

    Changed log file location

--- renfepy/logger.py
+++ renfepy/logger.py
@@ -2,7 +2,7 @@ import logging
 
 # Config logging
 logging.basicConfig(
-    filename="/tmp/renfe_search.log",
+    filename="/var/log/renfe_search.log",
     filemode="a",
     format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
     datefmt="%H:%M:%S",
